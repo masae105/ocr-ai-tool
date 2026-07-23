d9205bd4b43a2093b45185b50e4675c6568a656d
masae105 <ma19890105@gmail.com>
2026-07-24 04:42:35 +0900

    Test loader file loading

--- src/main.py
+++ src/main.py
@@ -1,11 +1,16 @@
+print("main.py start")
+
+
 from loader import load_file
 
 
 def main():
 
-    file_path = "../sample_data/images/test.png"
+    file_path = "sample_data/images/test.png"
 
     file = load_file(file_path)
+
+    print("読み込み成功:", file)
 
 
 if __name__ == "__main__":
